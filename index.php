c2506678cebe19366c7baeaac9a94cc2e9d0afcc
thomasphiri <91911754+thomasphiri@users.noreply.github.com>
2021-10-28 23:35:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<head>
+ <link rel="stylesheet" href="style.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ <?php echo '<link rel="stylesheet type="txt/css" href="file:///C:/xampp/htdocs/oer/fontawesome/css/font-awesome.css">';?>
+</head>
+
+<br><br>
+
+<input type="file" id="upload" hidden/>
+<label class="btn2" for="upload" style="float: right; background-color:red;letter-spacing: 3px; ">Go to Moodle <i class="fa fa-link"></i></label>
+<br><br>
+<br><br>
+<input type="file" id="upload" hidden/>
+<label class="btn2" for="upload"><i class="fa fa-file"></i>ICT 3020</label>
+
+<br><br><br><br>
+<input type="file" id="upload" hidden/>
+    <label class="btn2" for="upload"><i class="fa fa-folder fa-lg"></i></label> UPLOAD 
+    <br><br><br><br>
+    <input type="file" id="upload" hidden/>
+    <label class="btn2" for="upload"><i class="fa fa-edit fa-lg"></i></label> UPDATE
+
+
+    <br><br><br><br>
+    <input type="send" id="upload" hidden/>
+    <label class="btn2" for="upload"><i class="fa fa-share fa-lg"></i></label> SHARE
+
+
