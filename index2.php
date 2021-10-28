3b46df99f6d3de9a8529cba53543a24941b4c279
linda-5 <58687636+linda-5@users.noreply.github.com>
2021-10-28 22:37:39 +0100

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,41 @@
+
+<head>
+    <link rel="stylesheet" href="style.css">
+      <?php echo '<link rel="stylesheet type="txt/css" href="file:///C:/xampp/htdocs/oer/fontawesome/css/font-awesome.css">';?>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+   <nav>
+   <button class="btn"><a href="index.html"><i class="fa fa-home fa-lg" ></a></i><br>home</button>
+   
+   <label class="logo" style="color:white;" >Open Educationnal Resources</label>
+   
+   <ul>
+       <li><i class="fa fa-search fa-lg"></i> <input placeholder="search" style="line-height: 7px; padding: 12px 16px; border-radius: 10px;"></li>
+       
+       </ul>
+   </nav>
+   <br><br>
+   
+   <input type="file" id="upload" hidden/>
+   <label class="btn2" for="upload" style="float: right; background-color:red;letter-spacing: 3px; ">Go to Moodle <i class="fa fa-link"></i></label>
+   <br><br>
+       
+   <br><br>
+   <input type="file" id="upload" hidden/>
+   <label class="btn2" for="upload"><i class="fa fa-file"></i>ICT 3020</label>
+   
+   <br><br><br><br>
+   <center>
+   <input type="file" id="upload" hidden/>
+       <label class="btn2" for="upload"><i class="fa fa-upload fa-lg"></i></label> ADD ASSIGNMENT
+       <br><br><br><br>
+       <input type="file" id="upload" hidden/>
+       <label class="btn2" for="upload"><i class="fa fa-upload fa-lg"></i></label>ADD TOPIC
+   
+    </center>
+   <div class="footer">
+       <input type="file" id="upload" hidden/>
+       <label class="btn2" for="upload" style="float: right;"><i class="fa fa-save fa-lg"></i></label><P style="float: right;">SAVE</P>
+       <p>OER PLUGIN @ 2021 ICT CLASS, UNIVERSITY OF ZAMBIA ALL RIGHTS RESERVED</p>
+       
+     </div>
