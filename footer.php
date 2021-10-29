1e5c831c148b1617eefcd97ca94fa826cf6d25d5
mwenyacha <93401784+mwenyacha@users.noreply.github.com>
2021-10-29 19:09:01 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<div class="footer">
+    <input type="file" id="upload" hidden/>
+    <label class="btn2" for="upload" style="float: right;"><i class="fa fa-save fa-lg"></i></label><P style="float: right;">SAVE</P>
+    <p>OER PLUGIN @ 2021 ICT CLASS, UNIVERSITY OF ZAMBIA ALL RIGHTS RESERVED</p>
+    
+  </div>
---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav>
+<button class="btn"><a href="index.html"><i class="fa fa-home fa-lg" ></a></i><br>home</button>
+<label class="logo" style="color:white; font-size:35px; font-family:sans-serif;" >Open Educationnal Resources</label>
+
+
+<ul>
+    <li><i class="fa fa-pencil fa-lg"></i> <input placeholder="search" style="line-height: 7px; padding: 12px 16px; border-radius: 10px;"></li>
+    
+    </ul>
+</nav>
