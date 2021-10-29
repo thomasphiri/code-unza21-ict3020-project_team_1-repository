d4d1cf53b411feeb573474743ccf91c2ef66054b
Diana2497 <92678128+Diana2497@users.noreply.github.com>
2021-10-29 14:24:16 -0700

    Add files via upload

---
+++ index4.php
@@ -0,0 +1,16 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+      <?php echo '<link rel="stylesheet type="txt/css" href="file:///C:/xampp/htdocs/oer/fontawesome/css/font-awesome.css">';?>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+   <nav>
+   <button class="btn"><a href="index.html"><i class="fa fa-home fa-lg" ></a></i><br>home</button>
+   
+   <label class="logo" style="color:white;" >Open Educationnal Resources</label>
+   
+   <ul>
+       <li><i class="fa fa-search fa-lg"></i> <input placeholder="search" style="line-height: 7px; padding: 12px 16px; border-radius: 10px;"></li>
+       
+       </ul>
+   </nav>
+   <br><br>
